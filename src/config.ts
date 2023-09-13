2a423a6786a236b290edd6640f7c04b0173a8f87
Lazar <Lazar__93@hotmail.com>
2023-09-14 00:14:39 +0200

    Fix IR Posman address in mainnet config

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ const config = new TypedMap<string, TypedMap<string, string>>();
 const mainnetConfig = new TypedMap<string, string>();
 mainnetConfig.set('stETH', '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84');
 mainnetConfig.set('rETH', '0xae78736cd615f374d3085123a210448e74fc6393');
-mainnetConfig.set('interestRatePositionManager', ''); // Update once deployed
+mainnetConfig.set('interestRatePositionManager', '0x9AB6b21cDF116f611110b048987E58894786C244');
 
 const goerliConfig = new TypedMap<string, string>();
 goerliConfig.set('stETH', '0x1643E812aE58766192Cf7D2Cf9567dF2C37e9B7F');
