fb4d384231fadd7a93be488c29a6b54cabb18750
ShawTim <shawtim.liu@gmail.com>
2023-10-20 02:28:59 +0800

    support claiming event for RAFT staking (#35)

--- src/config.ts
+++ src/config.ts
@@ -6,12 +6,14 @@ const mainnetConfig = new TypedMap<string, string>();
 mainnetConfig.set('stETH', '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84');
 mainnetConfig.set('rETH', '0xae78736cd615f374d3085123a210448e74fc6393');
 mainnetConfig.set('R', '0x183015a9ba6ff60230fdeadc3f43b3d788b13e21');
+mainnetConfig.set('RAFT_BPT', '0xe91888a1d08e37598867d213a4acb5692071bb3a');
 mainnetConfig.set('interestRatePositionManager', '0x9AB6b21cDF116f611110b048987E58894786C244');
 mainnetConfig.set('0xe2eb229e88f56691e96bb98256707bc62160fe73', '15971525489660198786'); // CCIPOnRampContract -> destinationChainSelector
 
 const goerliConfig = new TypedMap<string, string>();
 goerliConfig.set('stETH', '0x1643E812aE58766192Cf7D2Cf9567dF2C37e9B7F');
 goerliConfig.set('rETH', '0x0b26a03413aCca79eE539015f036B7dF79ddD1c5');
+goerliConfig.set('RAFT_BPT', '0xf8a0623ab66f985effc1c69d05f1af4badb01b00');
 goerliConfig.set('interestRatePositionManager', '0x7b07fc0c5829fc8c74cc18df99c50b7d7fe830b4');
 
 const baseConfig = new TypedMap<string, string>();
--- src/staking.ts
+++ src/staking.ts
@@ -1,16 +1,27 @@
 import { StakingTransaction } from '../generated/schema';
 import { Deposit, Withdraw } from '../generated/VotingEscrow/VotingEscrow';
+import { TokensClaimed } from '../generated/FeeDistributor/FeeDistributor';
 import { loadPosition } from './positions';
+import { config } from './config';
+import { dataSource } from '@graphprotocol/graph-ts';
 
 const DEPOSIT_TYPES = ['DEPOSIT_FOR', 'CREATE_LOCK', 'INCREASE_LOCK_AMOUNT', 'INCREASE_UNLOCK_TIME', 'WITHDRAW'];
 
 export function handleDeposit(event: Deposit): void {
+  const networkConfig = config.get(dataSource.network());
+
   const transactionHash = event.transaction.hash.toHexString();
   const provider = event.params.provider.toHexString().toLowerCase();
   const type = DEPOSIT_TYPES[event.params.deposit_type];
   const amount = event.params.value;
   const unlockTime = event.params.locktime;
   const timestamp = event.params.ts;
+  let token = '';
+
+  if (networkConfig !== null) {
+    const tokenAddress = networkConfig.get('RAFT_BPT');
+    token = tokenAddress !== null ? (token as string) : '';
+  }
 
   const position = loadPosition(provider);
 
@@ -18,6 +29,7 @@ export function handleDeposit(event: Deposit): void {
   transaction.type = type;
   transaction.position = position.id;
   transaction.provider = provider;
+  transaction.token = token;
   transaction.amount = amount;
   transaction.unlockTime = unlockTime;
   transaction.timestamp = timestamp;
@@ -26,10 +38,18 @@ export function handleDeposit(event: Deposit): void {
 }
 
 export function handleWithdraw(event: Withdraw): void {
+  const networkConfig = config.get(dataSource.network());
+
   const transactionHash = event.transaction.hash.toHexString();
   const provider = event.params.provider.toHexString().toLowerCase();
   const amount = event.params.value;
   const timestamp = event.params.ts;
+  let token = '';
+
+  if (networkConfig !== null) {
+    const tokenAddress = networkConfig.get('RAFT_BPT');
+    token = tokenAddress !== null ? (token as string) : '';
+  }
 
   const position = loadPosition(provider);
 
@@ -37,6 +57,27 @@ export function handleWithdraw(event: Withdraw): void {
   transaction.type = 'WITHDRAW';
   transaction.position = position.id;
   transaction.provider = provider;
+  transaction.token = token;
+  transaction.amount = amount;
+  transaction.timestamp = timestamp;
+  transaction.save();
+  position.save();
+}
+
+export function handleClaim(event: TokensClaimed): void {
+  const transactionHash = event.transaction.hash.toHexString();
+  const user = event.params.user.toHexString().toLowerCase();
+  const token = event.params.token.toHexString().toLowerCase();
+  const amount = event.params.amount;
+  const timestamp = event.params.userTokenTimeCursor;
+
+  const position = loadPosition(user);
+
+  const transaction = new StakingTransaction(transactionHash);
+  transaction.type = 'CLAIM';
+  transaction.position = position.id;
+  transaction.provider = user;
+  transaction.token = token;
   transaction.amount = amount;
   transaction.timestamp = timestamp;
   transaction.save();
